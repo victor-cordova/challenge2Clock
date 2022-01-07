86b324626fdc36d9198dfbd789a4571240b4c95f
victor_cordova <victorcordovapoma@gmail.com>
2022-01-06 23:11:12 -0500

    Fixed the problem with hour from 24 to 12.

--- index.js
+++ index.js
@@ -9,21 +9,33 @@ const second = date.getSeconds();
 const minute = date.getMinutes();
 const hour = date.getHours();
 
-const hourGap = (minute*(360/12))/60;
+const hourGap = (minute * (360 / 12)) / 60;
+
+const transformHour = (hour) => { 
+    if (hour <= 12) {
+        return hour;
+    }
+    return hour - 12;
+}
+
+const hourTrasformed = transformHour(hour);
 
 clockHandSecond.style.cssText = `transform: rotate(${(second * 360) / 60}deg);`;
 clockHandMinute.style.cssText = `transform: rotate(${(minute * 360) / 60}deg);`;
-clockHandHour.style.cssText = `transform: rotate(${(hour * 360) / 12 + hourGap }deg);`;
 
+clockHandHour.style.cssText = `transform: rotate(${(hourTrasformed * 360) / 12 + hourGap}deg);`;
 
 setInterval( () => {
     const date = new Date();
     const second = date.getSeconds();
     const minute = date.getMinutes();
+    const hour = date.getHours();
+
+    const hourTrasformed = transformHour(hour);
 
     const hourGap = (minute*(360/12))/60;
 
     clockHandSecond.style.cssText = `transform: rotate(${(second * 360) / 60}deg);`;
     clockHandMinute.style.cssText = `transform: rotate(${(minute * 360) / 60}deg);`;
-    clockHandHour.style.cssText = `transform: rotate(${(hour * 360) / 12 + hourGap}deg);`;
+    clockHandHour.style.cssText = `transform: rotate(${(hourTrasformed * 360) / 12 + hourGap}deg);`;
 }, 1000);
